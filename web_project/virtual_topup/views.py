a6fbd386b7a7240927c931705114d333d77fa1a1
Adedayo Ogunleye <adedayoogunleye2@gmail.com>
2024-02-13 15:03:34 +0100

    My project

--- web_project/virtual_topup/views.py
+++ web_project/virtual_topup/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+class referalView(TemplateView):
+    template_name = 'referal.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(referalView, self).get_context_data(**kwargs)
+        context['referal'] = Referal_list.objects.filter(
+            user=self.request.user)
+        context['referal_total'] = Referal_list.objects.filter(
+            user=self.request.user).count()
+
+        return context

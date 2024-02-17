87abf50c313034f995de41811b27b0ced8e436f9
Adedayo Ogunleye <adedayoogunleye2@gmail.com>
2024-02-17 18:56:36 +0100

    Removed

--- website/myproject/virtual_topup/template/index.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector('.hamburger')
-let sideBar = document.querySelector('#sidebar')
-
-window.onscroll = () => {
-    sideBar.classList.remove('active')
-}
-menu.addEventListener('click', () => {
-    sideBar.classList.toggle('active')
-})

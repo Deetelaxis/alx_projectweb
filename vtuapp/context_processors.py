bda3fb0f65dbd2a241ff1ac9b7b92607155c3c06
Deetelaxis <122943124+Deetelaxis@users.noreply.github.com>
2024-02-19 12:54:57 +0100

    Another replicate

--- vtuapp/context_processors.py
+++
@@ -1,6 +0,0 @@
-from.models import *
-
-def categories_processor(request):
-    # Your logic to retrieve categories goes here
-    categories = [...]  # Replace [...] with your actual logic to retrieve categories
-    return {'categories': categories}
